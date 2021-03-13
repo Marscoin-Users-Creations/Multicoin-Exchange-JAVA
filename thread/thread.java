f072e9d1fafd324c075a0bf1c6f2ced2c188f81c
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-13 23:39:04 +0100

    Created thread.java

---
+++ thread/thread.java
@@ -0,0 +1,15 @@
+// default imports
+
+
+
+
+// thread vars
+
+public class thread() {
+    
+    Thread thread1 = new Thread();
+    Thread thread2 = new Thread();
+    Thread thread3 = new Thread();
+    Thread thread4 = new Thread();
+    
+};
