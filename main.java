6668f7f390e475832fdbf52db4552cf29b8b9081
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-13 19:09:40 +0100

    Created main.java

---
+++ main.java
@@ -0,0 +1,53 @@
+// default imports
+
+import java.lang.Thread;
+import java.net.*;
+
+
+// main vars
+
+public class vars {
+    
+    public static internetVars(String[] args) {
+        
+        int bitcoinInternetAddress = "127.0.0.1";
+        int bitcoinInternetPort = 0;
+        int bitcoinCashInternetAddress = "127.0.0.1";
+        int bitcoinCashInternetPort = 0;
+        int litecoinInternetAddress = "127.0.0.1";
+        int litecoinInternetPort = 0;
+        int marscoinInternetAddress = "127.0.0.1";
+        int marscoinInternetPort = 0;
+        
+    };
+    
+};
+
+
+// main class
+
+public class main {
+    
+    public static void internet(String[] args) {
+        
+        Socket bitcoinInternetSocket = new Socket(InetAddress vars.internetVars.bitcoinInternetAddress, int vars.internetVars.bitcoinInternetPort);
+        Socket bitcoinCashInternetSocket = new Socket(InetAddress vars.internetVars.bitcoinCashInternetAddress int vars.internetVars.bitcoinCashInternetPort);
+        Socket litecoinInternetSocket = new Socket(InetAddress vars.internetVars.litecoinInternetAddress, int vars.internetVars.litecoinInternetPort);
+        Socket marscoinInternetSocket = new Socket(InetAddress vars.internetVars.marscoinInternetAddress, int vars.internetVars.marscoinInternetPort);
+        
+    };
+    public static void Main(String[] args) {
+        
+        
+        
+    };
+    public static void threading(String[] args) {
+        
+        Thread thread1 = new Thread();
+        Thread thread2 = new Thread();
+        Thread thread3 = new Thread();
+        Thread thread4 = new Thread();
+        
+    };
+    
+};
