c30bb21868759b51a6cec291ce3939a82b1d0f2a
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-14 00:06:28 +0100

    Created database.java

---
+++ database/database.java
@@ -0,0 +1,27 @@
+// default imports
+
+
+
+
+// default packages
+
+
+
+
+// database class
+
+public class database {
+    
+    public static void db(String[] args) {
+        
+        int bannedIps = [];
+        int kickedIps = {
+            
+            
+            
+        };
+        int whitelistIps = [];
+        
+    };
+    
+};
